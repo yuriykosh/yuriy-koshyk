f7ed97cd91feb157c8728fde29235f7e92c3968a
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2024-10-02 00:11:59 +0200

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,17 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const satoshi = localFont({
+  src: [
+    {
+      path: "./fonts/Satoshi-Variable.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Satoshi-VariableItalic.woff2",
+      style: "italic",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -25,11 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${satoshi.className} antialiased`}>{children}</body>
     </html>
   );
 }
